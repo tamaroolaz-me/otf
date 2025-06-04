9a60c3fcfaaa4d8a2d213cf7f9fa9e7be47e4e5d
Tamryn Roberts <tamrynroberts@Tamryns-MacBook-Pro.local>
2025-06-04 11:41:36 +0200

    Add Google Analytics tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,24 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7T28RDR5PC"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7T28RDR5PC');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
